e677ecb60cd5b73a0c0a4adacc0875a8485334d8
M-Mks <matteo.mikos@seascapebelgium.be>
2025-01-30 15:20:34 +0100

    test

--- assets/helper_functions.py
+++ assets/helper_functions.py
@@ -19,46 +19,6 @@ def create_pie_chart(df, question):
     )
     return html.Div([title_html, dcc.Graph(figure=fig)])
 
-def create_multi_select_histogram(df, question):
-    hist_df = df[question].dropna().str.split(",").explode().str.strip()
-    unique_hist = sorted(hist_df.unique())
-    hist_counts = pd.DataFrame({
-        "a": unique_hist,
-        "b": [hist_df.tolist().count(opt) for opt in unique_hist] 
-    })
-    # Rename columns
-    hist_counts.columns = ["a", "b"]
-    hist_counts["b"] = pd.to_numeric(hist_counts["b"])  # Ensure 'b' is numeric
-    
-    print(unique_hist)
-    fig = px.bar(
-        hist_counts, x="a", y="b", 
-        color="a", 
-        category_orders={"a": unique_hist}
-    )
-    print(hist_counts["b"].dtype)
-    ''''
-    fig.update_traces(marker_color=sample_colorscale("RdYlGn", hist_counts["a"] / hist_counts["b"].max()))
-    '''
-    
-    fig.update_layout(
-        title=None, 
-        xaxis_title="Options",
-        yaxis_title="Count",
-        legend=GRAPH_LAYOUT["legend"], 
-        **GRAPH_LAYOUT["general"]
-    )
-
-    title_html = html.Div(
-        f"{question}".replace("/", "<br>"),
-        style={
-            'textAlign': 'center', 'fontSize': '20px', 'color': '#1f2a44',
-            'fontFamily': 'Helvetica, Arial, sans-serif', 'fontWeight': 'normal', 'marginBottom': '2px'
-        }
-    )
-
-    return html.Div([title_html, dcc.Graph(figure=fig)])
-
 def create_numeric_pie_chart(df, question, value_mapping, category_order):
     # Map the values using the value_mapping and handle missing values
     mapped_pie = df[question].map(value_mapping).fillna("Unknown")
@@ -69,7 +29,7 @@ def create_numeric_pie_chart(df, question, value_mapping, category_order):
     vc["b"] = pd.to_numeric(vc["b"])
 
 
-    print(vc)  # Debugging
+    print(vc.dtypes)  # Debugging
 
     # Create the pie chart
     fig = px.pie(
@@ -93,6 +53,42 @@ def create_numeric_pie_chart(df, question, value_mapping, category_order):
         }
     )
     
+    return html.Div([title_html, dcc.Graph(figure=fig)])
+
+def create_multi_select_histogram(df, question):
+    hist_df = df[question].dropna().str.split(",").explode().str.strip()
+    unique_hist = sorted(hist_df.unique())
+    hist_counts = pd.DataFrame({
+        "a": unique_hist,
+        "b": [hist_df.tolist().count(opt) for opt in unique_hist] 
+    })
+    # Rename columns
+    hist_counts.columns = ["a", "b"]
+    hist_counts["b"] = pd.to_numeric(hist_counts["b"])  # Ensure 'b' is numeric
+        
+    fig = px.bar(
+        hist_counts, x="a", y="b", 
+        color="a", 
+        category_orders={"a": unique_hist}
+    )
+
+    fig.update_traces(marker_color=sample_colorscale("RdYlGn", hist_counts["b"] / hist_counts["b"].max()))    
+    fig.update_layout(
+        title=None, 
+        xaxis_title="Options",
+        yaxis_title="Count",
+        legend=GRAPH_LAYOUT["legend"], 
+        **GRAPH_LAYOUT["general"]
+    )
+
+    title_html = html.Div(
+        f"{question}".replace("/", "<br>"),
+        style={
+            'textAlign': 'center', 'fontSize': '20px', 'color': '#1f2a44',
+            'fontFamily': 'Helvetica, Arial, sans-serif', 'fontWeight': 'normal', 'marginBottom': '2px'
+        }
+    )
+
     return html.Div([title_html, dcc.Graph(figure=fig)])
 
 def create_ordered_pie_chart(df, question, category_order):
