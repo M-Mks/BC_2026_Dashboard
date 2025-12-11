2574095ff8cbd29bce6933ff422ca99915c47775
M-Mks <143797701+M-Mks@users.noreply.github.com>
2025-12-11 10:54:15 +0100

    renamed the server

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ last_update_date = mod_info["csv_last_modified"]
 
 # Initialize the Dash app
 dash_app = Dash(__name__)
-server = dash_app.server
+app = dash_app.server
 dash_app.title = "Survey Results Dashboard"
 
 # Graph style definition
