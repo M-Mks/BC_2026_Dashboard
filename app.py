8e5941b1a6cfcc2c9cc7171abd6291eb0d85cae4
M-Mks <143797701+M-Mks@users.noreply.github.com>
2025-12-11 11:14:34 +0100

    update to dash app

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ last_update_date = mod_info["csv_last_modified"]
 # Initialize the Dash app
 dash_app = Dash(__name__)
 app = dash_app.asgi_app
+
 dash_app.title = "Survey Results Dashboard"
 
 # Graph style definition
